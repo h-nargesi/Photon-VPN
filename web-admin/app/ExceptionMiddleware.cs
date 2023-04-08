07912b24a608950d941abad206b203b4e7af9c5d
hamedna <hamedna@systemgroup.net>
2023-04-08 17:28:04 +0330

    Exception handler

--- web-admin/app/ExceptionMiddleware.cs
+++ web-admin/app/ExceptionMiddleware.cs
@@ -1,5 +1,26 @@
-﻿namespace Photon.Service.VPN.App;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Serilog;
+using System.Net;
 
-public class ExceptionMiddleware
+namespace Photon.Service.VPN.App;
+
+public static class ExceptionMiddleware
 {
+    public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseWhen(context => context.Request.Path.StartsWithSegments("api") || context.Request.Path.StartsWithSegments("srv"),
+                    appJson => appJson.UseExceptionHandler(appError => appError.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.ContentType = "application/json";
+                        var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                        if (contextFeature != null)
+                        {
+                            Log.Fatal(contextFeature.Error.StackTrace ?? string.Empty);
+                            await context.Response.WriteAsync("Internal Server Error.");
+                        }
+                    })));
+
+        app.UseExceptionHandler();
+    }
 }
