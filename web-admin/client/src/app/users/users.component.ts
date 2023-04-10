bcf1487f7090f7f6f4ad83352f621e5f42fedfbd
hamedna <hamedna@systemgroup.net>
2023-04-10 14:00:12 +0330

    Load Users Data

--- web-admin/client/src/app/users/users.component.ts
+++ web-admin/client/src/app/users/users.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
 })
-export class UsersComponent {
-  constructor(title: Title) {
+export class UsersComponent implements OnInit {
+  constructor(
+    title: Title,
+    private service: UsersService) {
     title.setTitle("Users | Photon - VPN");
+  }
+
+  ngOnInit(): void {
+    // this.service.Users.List();
   }
 }
