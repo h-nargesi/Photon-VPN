cbd78919286aa459b11641be3b3bc7ff612a2c55
Hamed Nargesi <hamd.nargesi.jar@gmail.com>
2023-04-12 15:01:14 +0330

    script to change password

--- database/load-entity.sh
+++ database/load-entity.sh
@@ -1,2 +1,4 @@
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON rd.* to 'photon'@'31.2.225.160' IDENTIFIED BY 'znf42CYfWhA3st7w';"
+
 mysql -u photon -h 37.32.9.55 -p < database.sql
 dotnet ef dbcontext scaffold "server=37.32.9.55;uid=photon;pwd=znf42CYfWhA3st7w;database=rd" MySql.EntityFrameworkCore -o models -f
