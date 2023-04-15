007e7ff65ca45bef758f26c0659186bd7c5b9871
Hamed Nargesi <hamd.nargesi.jar@gmail.com>
2023-04-15 21:36:07 +0330

    select users

--- database/load-entity.sh
+++ database/load-entity.sh
@@ -1,3 +1,6 @@
+
+mysql -u root mysql -e "select user, host from user;"
+
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON rd.* to 'photon'@'31.2.225.160' IDENTIFIED BY 'znf42CYfWhA3st7w';"
 
 mysql -u photon -h 37.32.9.55 -p < database.sql
