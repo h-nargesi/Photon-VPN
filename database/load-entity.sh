cc933494f44777cd16cddd7e9900a77373044df8
Hamed Nargesi <hamd.nargesi.jar@gmail.com>
2023-04-22 01:22:50 +0330

    mysql user ccess

--- database/load-entity.sh
+++ database/load-entity.sh
@@ -1,7 +1,8 @@
 
-mysql -u root mysql -e "select user, host from user;"
-
-sudo mysql -u root -e "GRANT ALL PRIVILEGES ON rd.* to 'photon'@'31.2.225.160' IDENTIFIED BY 'znf42CYfWhA3st7w';"
+sudo mysql -u root mysql -e "select user, host from user;"
+sudo mysql -u root mysql -e "drop user 'photon'@'5.217.150.199';"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON rd.* to 'photon'@'5.217.150.199' IDENTIFIED BY 'znf42CYfWhA3st7w';"
 
 mysql -u photon -h 37.32.9.55 -p < database.sql
+
 dotnet ef dbcontext scaffold "server=37.32.9.55;uid=photon;pwd=znf42CYfWhA3st7w;database=rd" MySql.EntityFrameworkCore -o models -f
